f9c14f1d62d3134f4dfff58d68f4cc694d7c4cbd
looth017 <looth017@gamil.com>
2018-10-18 21:43:43 +0500

    Added migration files for items, orders and tags

---
+++ database/migrations/2014_10_12_400000_create_items_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->int('merchant_id')->unique();
+            $table->string('name');
+            $table->string('description');
+            $table->floatval('price');
+            $table->string('status');
+            $table->string('featured_image');
+            $table->int('rating');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
---
+++ database/migrations/2014_10_12_500000_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->int('merchant_id')->unique();
+            $table->int('customer_id')->unique();
+            $table->string('status');
+            $table->floatval('total_price');
+            $table->floatval('cash_balance');
+            $table->string('delivery_type');
+            $table->string('payment_type');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
